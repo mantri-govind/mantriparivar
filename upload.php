7b3e4e957ce5c5d6c884b2a9316c52d0a0586595
Govind Mantri <mantri.govind@hotmail.com>
2017-07-29 22:06:36 +0530

    some changes

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@
 <form action="save.php" method="post" enctype="multipart/form-data">
    कृपया छोटी साइज की फोटो भेजे। फोटो का नाम सदस्य के नाम का अग्रिम शब्द  व पिता का नाम  रखे उदारण के लिए अगर आप का नाम सूचि मैं गोविन्द है तो फोटो का नाम "Govind-Champalal" रखे ताकि नाम ढूढ़ने मैं आसानी हो।  
     <input type="file" name="fileToUpload" id="fileToUpload" required>
-    <input type="file" name="name" id="name_of_person" required>
+    <input type="text" name="name" id="name_of_person" required>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
